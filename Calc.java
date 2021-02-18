5c4a222fd227d5a374dba3737284a73b0ef5e0c0
franl08 <a93202@alunos.uminho.pt>
2021-02-18 10:55:52 +0000

    Added calculator to integers

---
+++ Calc.java
@@ -0,0 +1,50 @@
+public class Calc {
+    public static void main(String[] args){
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt (args[2]);
+        if (args[1].equals("+")){
+            sumTwoNumbers(num1,num2);
+        }
+        else if (args[1].equals("-")){
+            subtwoNumbers(num1, num2);
+        }
+        else if (args[1].equals("*")){
+            multTwoNumbers(num1, num2);
+        }
+        else if (args[1].equals("/")){
+            divTwoNumbers(num1, num2);
+        }
+        else if(args[1].equals("%")){
+            remainderTwoNumbers (num1, num2);
+        }
+        else{
+            System.out.println("Invalid sentence");
+            return;
+        }
+
+    }
+    public static void sumTwoNumbers (int a, int b){
+        int r = a + b;
+        System.out.println(a + " + " + b + " = " + r);
+    }
+    public static void subtwoNumbers (int a, int b){
+        int r = a - b;
+        System.out.println(a + " - " + b + " = " + r);
+    }
+    public static void multTwoNumbers(int a, int b){
+        int r = a * b;
+        System.out.println(a + " * " + b + " = " + r);
+    }
+    public static void divTwoNumbers(int a, int b){
+        try{
+            int r = a / b;
+            System.out.println(a + " / " + b + " = " + r);
+        } catch(ArithmeticException  e){
+            System.out.println("Can't divide a number by zero");
+        }
+    }
+    public static void remainderTwoNumbers (int a, int b){
+        int r = a % b;
+        System.out.println(a + " % " +   b  +  " = " + r);
+    }
+}
